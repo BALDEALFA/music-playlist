8d687b2ffa23a5db138af2ff3b129e93a2f863a6
BALDE ALFA <alfaafro@gmail.com>
2021-03-12 21:25:28 +0000

    Connecting to Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCMNuRRgn2uMY4kC89aa_gbUJ6_bW85SZA",
+	authDomain: "music-playlist-7b15f.firebaseapp.com",
+	projectId: "music-playlist-7b15f",
+	storageBucket: "music-playlist-7b15f.appspot.com",
+	messagingSenderId: "301506379339",
+	appId: "1:301506379339:web:4e58d69bd58b14ba1ad7ce",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
