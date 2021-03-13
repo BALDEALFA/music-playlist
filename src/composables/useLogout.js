4204e4f299dcf4ff81be379443563e5c7d1ef809
BALDE ALFA <alfaafro@gmail.com>
2021-03-13 11:30:56 +0000

    Adding a 'pending' State to Requests

--- src/composables/useLogout.js
+++ src/composables/useLogout.js
@@ -1,24 +1,26 @@
-import { ref } from 'vue'
-import { projectAuth } from '../firebase/config'
+import { ref } from "vue";
+import { projectAuth } from "../firebase/config";
 
 // refs
-const error = ref(null)
+const error = ref(null);
+const isPending = ref(false);
 
 // logout function
 const logout = async () => {
-  error.value = null
-
-  try {
-    await projectAuth.signOut()
-  }
-  catch(err) {
-    console.log(err.message)
-    error.value = err.message
-  }
-}
+	error.value = null;
+	isPending.value = true;
+	try {
+		await projectAuth.signOut();
+		isPending.value = false;
+	} catch (err) {
+		console.log(err.message);
+		error.value = err.message;
+		isPending.value = false;
+	}
+};
 
 const useLogout = () => {
-  return { error, logout }
-}
+	return { error, logout, isPending };
+};
 
-export default useLogout
+export default useLogout;
