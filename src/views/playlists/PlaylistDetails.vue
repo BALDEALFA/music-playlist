0d06d5e287cfea04f236a79e53aa0419de3c4dc2
BALDE ALFA <alfaafro@gmail.com>
2021-03-17 22:37:15 +0000

    Adding Songs to Playlists

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -16,12 +16,14 @@
 
 		<!-- song list -->
 		<div class="song-list">
-			<p>song list here..</p>
+			<p>song list here</p>
+			<AddSong v-if="ownership" :playlist="playlist" />
 		</div>
 	</div>
 </template>
 
 <script>
+import AddSong from "@/components/AddSong.vue";
 import useStorage from "@/composables/useStorage";
 import useDocument from "@/composables/useDocument";
 import getDocument from "@/composables/getDocument";
@@ -30,6 +32,7 @@ import { computed } from "@vue/runtime-core";
 import { useRouter } from "vue-router";
 export default {
 	props: ["id"],
+	components: { AddSong },
 	setup(props) {
 		const { error, document: playlist } = getDocument("playlists", props.id);
 		const { user } = getUser();
